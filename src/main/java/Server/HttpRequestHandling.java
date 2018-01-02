42203f0a5db3a37f431e9556198e8232d73333d2
Saturnin Pugnet <saturnin.pugnet@transferwise.com>
2018-01-02 13:35:26 +0000

    testing autodeploy

--- src/main/java/Server/HttpRequestHandling.java
+++ src/main/java/Server/HttpRequestHandling.java
@@ -49,6 +49,8 @@ public class HttpRequestHandling {
 
             MiningConfiguration responseConfig = new CryptoCurrencyOptimalMiningConfigHandler().handle(dataExchangeMedium);
             String response = responseConfig.toJson().toString();
+            //TODO: remove next line
+            response = response + "test";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
