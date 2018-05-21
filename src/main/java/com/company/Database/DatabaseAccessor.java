359747434a64405a91d7e7489f56927b34ed7903
Satrnin Pugnet <saturnin.pugnet14@ic.ac.uk>
2018-05-21 15:56:46 +0100

    minor

--- src/main/java/com/company/Database/DatabaseAccessor.java
+++ src/main/java/com/company/Database/DatabaseAccessor.java
@@ -90,7 +90,7 @@ public class DatabaseAccessor {
         logger.info("Updating graphic card table with: " + workerName + ", " + currency + ", " + hashrate);
         // Create or update graphic_card
         String sql2 = "INSERT INTO graphic_cards (worker_id, graphic_card_id, mined_currency, hashrate, timestamp, graphic_card_name) " +
-                "VALUES ('" + worker_id + "', '" + gpu.getId() + "', '" + currency + "', " + hashrate + ", CURRENT_TIMESTAMP, " + gpu.getGraphicCardName() + ") " +
+                "VALUES ('" + worker_id + "', '" + gpu.getId() + "', '" + currency + "', " + hashrate + ", CURRENT_TIMESTAMP, '" + gpu.getGraphicCardName() + "') " +
                 "ON CONFLICT (worker_id, graphic_card_id) " +
                 "DO UPDATE SET worker_id='" + worker_id + "', " +
                 "    graphic_card_id='" + gpu.getId() + "', mined_currency='" + currency + "', hashrate=" + hashrate + ", timestamp=CURRENT_TIMESTAMP, graphic_card_name='" + gpu.getGraphicCardName() + "' " +
